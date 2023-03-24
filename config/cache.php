ffa41338e15020b1b6b6aa9f5b1bd4cc8d59f36d
刘晓彬 <563859671@qq.com>
2023-03-24 13:59:50 +0800

    调整配置

--- config/cache.php
+++ config/cache.php
@@ -11,13 +11,43 @@ return [
     // 缓存连接方式配置
     'stores'  => [
         // 文件缓存方式
-        'file'  => [
+        'file'      => [
+            'type'   => 'file',
             'prefix' => '',
         ],
         
         // Redis缓存方式
-        'redis' => [
+        'redis'     => [
+            'type'     => 'redis',
+            'prefix'   => '',
+            'host'     => '127.0.0.1',
+            'port'     => 6379,
+            'password' => '',
+            'select'   => 0,
+        ],
+        
+        // Memcache缓存方式
+        'memcache'  => [
+            'type'   => 'memcache',
             'prefix' => '',
+            'host'   => '127.0.0.1',
+            'port'   => 11211,
+        ],
+        
+        // Memcached缓存方式
+        'memcached' => [
+            'type'     => 'memcached',
+            'prefix'   => '',
+            'host'     => '127.0.0.1',
+            'port'     => 11211,
+            'username' => '',
+            'password' => '',
+        ],
+        
+        // Wincache缓存方式
+        'wincache'  => [
+            'type'   => 'wincache',
+            'prefix' => ''
         ]
     ],
 ];
