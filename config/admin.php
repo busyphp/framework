17867e9fef4e97967a1e0736bbfee209a997ce57
刘晓彬 <563859671@qq.com>
2023-06-28 14:00:53 +0800

    更新配置

--- config/admin.php
+++ config/admin.php
@@ -2,9 +2,9 @@
 return [
     'model' => [
         // 系统用户模型
-        'admin_user'   => [
+        'admin_user'    => [
             // 验证相关
-            // 'validate' => [
+            'validate' => [
                 // 头像是否必传
                 // 'avatar'   => false,
                 
@@ -12,7 +12,7 @@ return [
                 // 'nickname' => false,
                 
                 // 手机号选项
-                // 'phone'    => [
+                'phone'   => [
                     // 是否必填验证
                     // 'required' => false,
                     
@@ -21,7 +21,7 @@ return [
                     
                     // 前端校验手机号正则
                     // 'js_regex' => '^1[3-9]\d{9}$',
-                // ],
+                ],
                 
                 // 邮箱是否必填
                 // 'email'    => false,
@@ -30,7 +30,7 @@ return [
                 // 'name'     => false,
                 
                 // 证件号码选项
-                // 'card_no'  => [
+                'card_no' => [
                     // 是否必填验证
                     // 'required' => false,
                     
@@ -39,7 +39,7 @@ return [
                     
                     // 是否校验身份证号码
                     // 'identity' => false
-                // ],
+                ],
                 
                 // 性别是否必选
                 // 'sex'      => false,
@@ -48,7 +48,7 @@ return [
                 // 'birthday' => false,
                 
                 // 电话号码选项
-                // 'tel'      => [
+                'tel'     => [
                     // 是否必填验证
                     // 'required' => false,
                     
@@ -57,30 +57,30 @@ return [
                     
                     // 前端校验电话号码正则
                     // 'js_regex' => null,
-                // ],
-            // ],
+                ],
+            ],
             
             // 登录相关
-            // 'login'    => [
+            'login'    => [
                 // 解密通行 authKey 的秘钥，24位字符，默认为：Pe78mUtfomfhHqSHGpQ3jAlI
                 // 'auth_secret'           => '',
                 
                 // 通行token扩展
-                // 'auth_extend'           => [
+                'auth_extend' => [
                     // 创建扩展内容，生成token时调用
                     // 'create' => null,
                     
                     // 检测扩展内容，检测登录时调用，失败请抛出异常
                     // 'check'  => null
-                // ],
+                ],
                 
                 // 允许多设备同时登录的token生成扩展，支持闭包与字符串，闭包必须返回字符串
                 // 'multiple_client_token' => null,
-            // ],
+            ],
         ],
         
         // 用户通行模型
-        'system_token' => [
+        'system_token'  => [
             // 登录类型映射
             // 'type_map'      => [
             //     1 => '网页端',
@@ -95,24 +95,32 @@ return [
         ],
         
         // 文件模型
-        'system_file' => [
+        'system_file'   => [
             // 文件类型扩展
-            // 'file_types' => [
+            'file_type_map' => [
                 // 类型 => [
                 //     'name' => '类型名称',
                 //     'icon' => 'fa fa-file-o',
                 //     'types' => ['文件扩展名1', '文件扩展名2', '文件扩展名...']
                 // ]
-                // 'code' => [
-                    // 'name'  => '代码',
-                    // 'types' => ['html', 'js', 'php']
-                // ]
-            // ],
+            ],
             
             // 文件图标映射
-            // 'file_icons' => [
+            'file_icon_map' => [
                 // 格式 => '图标名称或图标路径'
-            // ]
+            ]
+        ],
+        
+        // 系统消息模型
+        'admin_message' => [
+            // 消息类型映射
+            'type_map' => [
+                // 类型 => [
+                //     'name' => '类型名称'
+                //     '自定义1' => ...
+                //     '自定义2' => ...
+                // ]
+            ]
         ]
     ]
 ];
